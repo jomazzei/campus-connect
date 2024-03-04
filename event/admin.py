7b7af7f02f93d8253e34dd557d08fa4887305d98
Joseph Mazzei <j.mazzei.pv@gmail.com>
2024-03-04 15:54:35 +0000

    Restart of event app, initial commited version could not access the event listings from admin panel

--- event/admin.py
+++ event/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
-from .models import Event
-from django_summernote.admin import SummernoteModelAdmin
 
-
-# @admin.register(Event)
-# class TextEditAdmin(SummernoteModelAdmin):
-    # !! This is where we'll add summernote fields after all the model fields are defined !!
-
-admin.site.register(Event)
+# Register your models here.
--- event/models.py
+++ event/models.py
@@ -1,28 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
 class Event(models.Model):
-    # Name of event
-    title = models.CharField(max_length=150, unique=True)
-    # URL for event post
-    slug = models.SlugField(max_length=150, unique=True)
-    # The associated user
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="event_posts"
-    )
-    # Main body of text
-    content = models.TextField()
-    # Time of creation
-    created_on = models.DateTimeField(auto_now_add=True)
-    # Time of update / edit of post
-    updated_on = models.DateTimeField(auto_now=True)
-
-    # Field for event hosting date
-    event_date = models.DateTimeField()
-    
-    # Sets image field and default image
-    image = CloudinaryField('image', default='placeholder')
-
+    # title = 
+    # event_host_date = 
+    # venue = 
+    # organizer = 
+    # content =
+    # max_attendance = 
+    # attendance-tracker =
+    # image = 
