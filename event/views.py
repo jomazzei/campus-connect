246bb90877023797a21fe0ee7a8ca7384f821316
Joseph Mazzei <j.mazzei.pv@gmail.com>
2024-03-05 13:25:36 +0000

    Added crispy forms package. Put Sherrif's HTML templates in a folder to be turned into Django templates. Wired working home view URL. Gave up on VSCode.

--- event/views.py
+++ event/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Event
+
 
 # Create your views here.
+class EventList(generic.ListView):
+    queryset = Event.objects.all()
+    template_name = "event_list.html"
