8ce1c7498666c732ceca3bb14680173142dab5f7
Daniel Favell <danfavell97@gmail.com>
2024-03-06 10:31:32 +0000

    extended base.html file

--- event/views.py
+++ event/views.py
@@ -7,3 +7,15 @@ from .models import Event
 class EventList(generic.ListView):
     queryset = Event.objects.all()
     template_name = "event_list.html"
+
+def CreateForm(request):
+    return render(
+        request,
+        "event/form_create_event.html",
+    ) 
+
+def AboutPage(request):
+    return render(
+        request,
+        "event/about.html",
+    )
