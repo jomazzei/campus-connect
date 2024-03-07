5447a03c5bf3734d55e3d2db0aa49f609a14385b
jomazzei <153187237+jomazzei@users.noreply.github.com>
2024-03-07 22:03:02 +0000

    Update settings.py

--- campusconnect/settings.py
+++ campusconnect/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # when pushed to GitHub it will set itself to False
 DEBUG = 'DEBUG' in os.environ
 
-ALLOWED_HOSTS = ['.gitpod.io', '.herokuapp.com',]
+ALLOWED_HOSTS = ['.gitpod.io', '.herokuapp.com','.ui.dev']
 CSRF_TRUSTED_ORIGINS = ['https://*.gitpod.io', 'https://*.herokuapp.com',]
 
 
